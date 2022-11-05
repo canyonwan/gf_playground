3d47c073b6c8046302817ad413b2e247ba288771
canyonwan <canyonwan@126.com>
2022-11-05 22:14:17 +0800

    feat: new banner api

--- api/v1/banner.go
+++ api/v1/banner.go
@@ -5,8 +5,8 @@ import "github.com/gogf/gf/v2/frame/g"
 // CreateBannerReq 新增轮播图
 type CreateBannerReq struct {
 	g.Meta   `path:"/banner" method:"post" tags:"轮播图" summary:"创建Banner"`
-	Url      string `json:"url" in:"body" v:"required|#轮播图地址必填" dc:"轮播图地址"`
-	JumpLink string `json:"jumpLink" in:"body" v:"required|#跳转链接必填" dc:"跳转链接"`
+	Url      string `json:"url" in:"body" v:"required#轮播图地址必填" dc:"轮播图地址"`
+	JumpLink string `json:"jumpLink" in:"body" v:"required#跳转链接必填" dc:"跳转链接"`
 	Sort     int    `json:"sort" in:"body" dc:"排序"`
 }
 
