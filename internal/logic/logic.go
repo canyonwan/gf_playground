3d89723658e7bb968b662176611dddd14d9dcc8f
canyonwan <canyonwan@126.com>
2023-01-05 22:01:42 +0800

    feat: new logic init

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -7,7 +7,9 @@ package logic
 import (
 	_ "gf_playground/internal/logic/account"
 	_ "gf_playground/internal/logic/banner"
+	_ "gf_playground/internal/logic/bizctx"
 	_ "gf_playground/internal/logic/data_statistics"
 	_ "gf_playground/internal/logic/position"
+	_ "gf_playground/internal/logic/session"
 	_ "gf_playground/internal/logic/todo"
 )
