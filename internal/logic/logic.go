b4c62c968f6a32d82b4d863e9898d5f590c69f4e
canyonwan <canyonwan@126.com>
2023-01-06 16:39:43 +0800

    feat: new login service

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -9,6 +9,7 @@ import (
 	_ "gf_playground/internal/logic/banner"
 	_ "gf_playground/internal/logic/bizctx"
 	_ "gf_playground/internal/logic/data_statistics"
+	_ "gf_playground/internal/logic/login"
 	_ "gf_playground/internal/logic/position"
 	_ "gf_playground/internal/logic/session"
 	_ "gf_playground/internal/logic/todo"
