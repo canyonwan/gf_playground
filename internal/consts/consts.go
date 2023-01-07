590aaea29557cfbfbaf357013449f9e63364a137
canyonwan <canyonwan@126.com>
2023-01-07 22:02:01 +0800

    feat: update constants

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,10 +1,14 @@
 package consts
 
 const (
-	ContextKey               = "ContextKey"         // 上下文变量存储键名
-	CaptchaDefaultName       = "CaptchaDefaultName" // 验证码默认存储空间名称
-	FileMaxUploadCountMinute = 10                   // 同一用户1分钟之内最大上传数量
-	LoginPath                = "/backend/login"     // 登录路由
+	ContextKey               = "ContextKey" // 上下文变量存储键名
+	FileMaxUploadCountMinute = 10           // 同一用户1分钟之内最大上传数量
+	AuthFailMsg              = "登录超时或认证失败"
+	LoginPath                = "/backend/login" // 登录路由
 	LogoutPath               = "/backend/logout"
-	//AuthExcludePaths         = []string{""}
+	GTokenAdminPrefix        = "Admin:" // gtoken登录 管理后台 前缀区分
+	CtxAdminId               = "CtxAdminId"
+	CtxAdminName             = "CtxAdminName"
+	CtxAdminIsAdmin          = "CtxAdminIsAdmin"
+	CtxAdminRoleIds          = "CtxAdminRoleIds"
 )
