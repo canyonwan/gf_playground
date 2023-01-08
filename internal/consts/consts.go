ed5b3e69acc7693e84af9d351eea833754f61b1a
canyonwan <canyonwan@126.com>
2023-01-08 17:20:25 +0800

    fix: 小程序端和管理端登录地址分离

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -4,11 +4,13 @@ const (
 	ContextKey               = "ContextKey" // 上下文变量存储键名
 	FileMaxUploadCountMinute = 10           // 同一用户1分钟之内最大上传数量
 	AuthFailMsg              = "登录超时或认证失败"
-	LoginPath                = "/backend/login" // 登录路由
-	LogoutPath               = "/backend/logout"
+	AdminLoginPath           = "/backend/login"  // 管理端登录路由
+	LoginPath                = "/frontend/login" // app端登录路由
+	AdminLogoutPath          = "/backend/logout"
+	LogoutPath               = "/frontend/logout"
 	GTokenAdminPrefix        = "Admin:" // gtoken登录 管理后台 前缀区分
 	CtxAdminId               = "CtxAdminId"
-	CtxAdminName             = "CtxAdminName"
-	CtxAdminIsAdmin          = "CtxAdminIsAdmin"
+	CtxAdminAccount          = "CtxAdminAccount"
+	CtxAdminIsSuperAdmin     = "CtxAdminIsSuperAdmin"
 	CtxAdminRoleIds          = "CtxAdminRoleIds"
 )
