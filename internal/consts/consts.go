0cf805a4aaef422ab114ae29300aa1ff72860702
canyonwan <canyonwan@126.com>
2023-01-08 20:46:05 +0800

    fix: 配置常量字段

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -4,10 +4,10 @@ const (
 	ContextKey               = "ContextKey" // 上下文变量存储键名
 	FileMaxUploadCountMinute = 10           // 同一用户1分钟之内最大上传数量
 	AuthFailMsg              = "登录超时或认证失败"
-	AdminLoginPath           = "/backend/login"  // 管理端登录路由
-	LoginPath                = "/frontend/login" // app端登录路由
-	AdminLogoutPath          = "/backend/logout"
-	LogoutPath               = "/frontend/logout"
+	AdminLoginPath           = "/login" // 管理端登录路由
+	LoginPath                = "/login" // app端登录路由
+	AdminLogoutPath          = "/logout"
+	LogoutPath               = "/logout"
 	GTokenAdminPrefix        = "Admin:" // gtoken登录 管理后台 前缀区分
 	CtxAdminId               = "CtxAdminId"
 	CtxAdminAccount          = "CtxAdminAccount"
