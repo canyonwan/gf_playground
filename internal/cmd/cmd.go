9729994deb50b6426cd56a6b238c53e68899beca
canyonwan <canyonwan@126.com>
2023-01-08 17:21:24 +0800

    feat: 小程序端和管理端分离,区分是否auth

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -3,14 +3,14 @@ package cmd
 import (
 	"context"
 	"gf_playground/internal/consts"
+	"gf_playground/internal/controller"
+	"gf_playground/internal/middleware"
 	"gf_playground/internal/service"
 	"github.com/goflyfox/gtoken/gtoken"
 
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
-
-	"gf_playground/internal/controller"
 )
 
 var (
@@ -24,52 +24,74 @@ var (
 				oai = s.GetOpenApi()
 			)
 			// OpenApi自定义信
-			oai.Info.Title = `Canyonwan的接口文档`
 			//oai.Config.CommonResponse = response.JsonRes{}
 			oai.Config.CommonResponseDataField = `Data`
-			loginFunc := loginAuth
 
 			// 开启gfToken
-			gfToken := gtoken.GfToken{
-				ServerName:       "gf_shop_demo",
+			gfAdminToken := gtoken.GfToken{
+				ServerName: "gf_admin_auth",
+				//CacheMode:       2,
+				MultiLogin:       true,
+				LoginPath:        consts.AdminLoginPath,
+				LoginBeforeFunc:  middleware.LoginBeforeAuth,
+				LoginAfterFunc:   middleware.LoginAfterAuth,
+				LogoutPath:       consts.AdminLogoutPath,
+				AuthFailMsg:      consts.AuthFailMsg,
+				AuthAfterFunc:    middleware.AuthAfterFunc,
+				AuthExcludePaths: g.SliceStr{"/backend/login"}, // 不拦截白名单
+			}
+
+			// 小程序前端 gfToken
+			gfFrontendToken := gtoken.GfToken{
+				ServerName: "gf_frontend_auth",
+				//CacheMode:       2,
 				MultiLogin:       true,
 				LoginPath:        consts.LoginPath,
+				LoginBeforeFunc:  middleware.LoginBeforeAuth,
+				LoginAfterFunc:   middleware.LoginAfterAuth,
 				LogoutPath:       consts.LogoutPath,
-				LoginBeforeFunc:  loginFunc,
-				AuthExcludePaths: g.SliceStr{"/backend/login", "/backend/logout"}, // 不拦截白名单
+				AuthFailMsg:      consts.AuthFailMsg,
+				AuthAfterFunc:    middleware.AuthAfterFunc,
+				AuthExcludePaths: g.SliceStr{}, // 不拦截白名单
 			}
 
 			s.Group("/v1", func(group *ghttp.RouterGroup) {
-				// 使用gfToken中间件
-				if err := gfToken.Middleware(ctx, group); err != nil {
-					panic(err)
-				}
 				group.Middleware(
 					ghttp.MiddlewareCORS,
 					service.Middleware().Ctx,
 					service.Middleware().ResponseHandler,
 				)
-				group.Bind(
-					controller.Banner,
-					controller.DataStatistics,
-					controller.Todo,
-					controller.Position,
-					controller.Account,
-					//controller.Login,
-				)
+
+				// 后台管理 全部都需要登录的
+				group.Group("/backend", func(backendGroup *ghttp.RouterGroup) {
+					// 使用gfToken中间件
+					if err := gfAdminToken.Middleware(ctx, group); err != nil {
+						panic(err)
+					}
+
+					backendGroup.Bind(
+						controller.Banner,
+						controller.DataStatistics,
+						controller.Position,
+						controller.Account,
+					)
+
+				})
+
+				// 小程序前端
+				group.Group("/frontend", func(frontendGroup *ghttp.RouterGroup) {
+					// 使用gfToken中间件
+					if err := gfFrontendToken.Middleware(ctx, group); err != nil {
+						panic(err)
+					}
+					frontendGroup.Bind(
+						controller.Todo,
+					)
+				})
+
 			})
 			s.Run()
 			return nil
 		},
 	}
 )
-
-func loginAuth(r *ghttp.Request) (string, interface{}) {
-	account := r.Get("account").String()
-	pwd := r.Get("password").String()
-	if account == "" || pwd == "" {
-		r.Response.WriteJson(gtoken.Fail("账号或密码错误"))
-		r.ExitAll()
-	}
-	return account, "1"
-}
